b5fe4d55f2f5da77704b4d3836b4e61ac25b60ad
Mohammad Aatif <aatif@ipost.com>
2018-08-25 19:47:19 +0530

    Clean the code to create dnd trash

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Lists from './Lists';
-import { observe } from './Game';
 
 const rootEl = document.getElementById('root');
-
-observe((cardPosition) => {   
-    ReactDOM.render(<Lists cardPosition={cardPosition} />, rootEl)
-})
+ReactDOM.render(<App />, rootEl)
